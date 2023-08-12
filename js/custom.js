4eca8890849050bd7f72ed4a69632b47f9aea1e9
jawad602 <jawadjamil602@gmail.com>
2023-08-13 00:12:56 +0500

    update

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $(".mobile-menu-button").on("click", function() {
+        if($("ul.main-menu").hasClass("show")) {
+            $("ul.main-menu").removeClass("show");
+        } else {
+            $("ul.main-menu").addClass("show");
+        }
+    })        
+});
+
+$(document).ready(function(){
+    $(".banner-slider-container").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+});
+});
+$(document).ready(function(){
+    $('.banner-slider-container').slick({
+      dots: true,
+      arrows: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
